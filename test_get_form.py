8d9fa0561d5554fa0f67d7567dc0fc295f0d73cb
AKunshin <reyiby2015@gmail.com>
2023-11-15 11:28:52 +0300

    Modify regex for date and phone

--- test_get_form.py
+++ test_get_form.py
@@ -12,16 +12,20 @@ def test_get_template():
         "Content-Type": "application/json",
     }
 
-    # data = {
-    #     "user_name": "Piter Parker",
-    #     "user_email": "spiderman@marvel.com",
-    #     "registration_date": "2023-11-14"
-
-    # }
     data = {
-        "registration_dat": "14-11-2023"
+        "user_name": "Piter Parker",
+        "user_email": "spiderman@marvel.com",
+        "registration_date": "2023-11-14",
+        # "text_comment": "Comment user",
+        "user_password": "Secret",
 
     }
+    # data = {
+    #     # "registration_dat": "14.11.2023"
+    #     # "registration_dat": "2023-11-14"
+    #     "client_phone": "+7 987 124 12 12"
+
+    # }
     response = client.post("/get_form/", data=json.dumps(data), headers=headers)
     print(response.text)
     assert response.status_code == 200
